ef1e5ca0d7e4d4b5b94d46a366b03edb38dd6b98
Md-Asraful-Islam-Subbir <93644909+Md-Asraful-Islam-Subbir@users.noreply.github.com>
2025-12-30 01:01:49 +0600

    single page

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -4,6 +4,10 @@ import React, { useState } from 'react'
 import { formatDistanceToNow } from 'date-fns';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { CheckCircle2, Heart, Loader2, MapPin, MessageCircle, Share2Icon, ShoppingCart, User2 } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 
 const page = () => {
@@ -15,7 +19,10 @@ const page = () => {
 
     const book = {
         _id: "1",
-        images: [],
+        images: ["https://images.unsplash.com/photo-1604866830893-c13cafa515d5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8b25saW5lJTIwc2VsbCUyMGJvb2tzfGVufDB8fDB8fHww",
+            "https://media.istockphoto.com/id/910384920/photo/kid-reading-near-locked-door.webp?a=1&b=1&s=612x612&w=0&k=20&c=J3FL4ZVORItw_bkLzlVo4WO-xUy22S7Qqbuq2xusNnc=",
+            "https://images.unsplash.com/photo-1492539438225-2666b2a98f93?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG9sZCUyMCUyMGJvb2tzfGVufDB8fDB8fHww"
+        ],
         title: "The Alchemist",
         category: "Reading Books (Novels)",
         condition: "Excellent",
@@ -35,10 +42,10 @@ const page = () => {
         seller: { name: "John Doe", contact: "1234567890" }
     }
 
-    const handleAddToCart = {
+    const handleAddToCart = (productId: string) => {
 
     }
-    const handleAddToWishlist = {
+    const handleAddToWishlist = (productId: string) => {
 
     }
     const bookImage = book?.images || [];
@@ -71,18 +78,194 @@ const page = () => {
                 </nav>
                 <div className='grid gap-8 md:grid-cols-2'>
                     <div className='space-y-4'>
-                        <div className='relative h-120 overflow-hidden rounded-lg border bg-white shadow-md'>
+                        <div className='relative h-100 overflow-hidden rounded-lg border bg-white shadow-md'>
                             <Image src={bookImage[selectedImage]} alt={book.title} fill className='object-contain' />
                             {
                                 calculateDiscount(book.price, book.finalPrice) > 0 && (
                                     <span className="absolute left-0 top-2 rounded-r-lg px-2 py-1 text-xs font-medium bg-orange-600/90 text-white hover:bg-orange-700">
                                         {calculateDiscount(book.price, book.finalPrice)}% Off
                                     </span>
-                                )
-                            }
+                                )}
+                        </div>
+                        <div className='flex gap-2 overflow-x-auto py-2'>
+                            {bookImage.map((images, index) => (
+                                <button
+                                    key={index}
+                                    onClick={() => setSelectedImage(index)}
+                                    className={`relative h-16 w-16 shrink-0 overflow-hidden rounded-lg border transition-all duration-200 ${selectedImage === index ? "ring-2 ring-primary scale-105" : "hover:scale-105"}`}
+                                >
+                                    <Image
+                                        src={images}
+                                        alt={`${book.title}  ${index + 1}`}
+                                        fill
+                                        className='object-cover'
+                                    />
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='space-y-6'>
+                        <div className='flex items-center justify-between'>
+                            <div className='space-y-2'>
+                                <h1 className='text-2xl font-bold'>
+                                    {book.title}
+                                </h1>
+                                <p className='text-sm text-muted-foreground'>Postd {formatDate(book.createdAt)}</p>
+                            </div>
+                            <div className='flex gap-2'>
+                                <Button variant='outline'size='sm' >
+                                     <Share2Icon className='h-4 w-4 mr-1' />
+                                    <span className='hidden md:inline'>Share</span>
+                                </Button>
+                                <Button variant='outline' size='sm' onClick={() => handleAddToWishlist(book._id)}>
+                                    <Heart className='h-4 w-4 mr-1 fill-red-500' />
+                                    <span className='hidden md:inline'>Add</span>
+                                </Button>
+                            </div>
+                        </div>
+                        <div className='space-y-4'>
+                            <div className='flex items-baseline gap-2'>
+                                <span className='text-3xl font-bold'>৳{book.finalPrice}</span>
+                                {
+                                    book.price && (
+                                        <span className='text-lg text-muted-foreground line-through'>৳{book.price}</span>
+                                    )
+                                }
+                                <Badge variant='secondary' className='text-green-600'>Shipping Available</Badge>
+                            </div>
+                            <Button className='w-60 py-6 bg-blue-700'>
+                                {
+                                    isAddToCart ? (
+                                        <>
+                                            <Loader2 className='animate-spin mr-2' size={20} /> Adding to Cart...
+                                        </>
+                                    ) :
+                                        <>
+                                            <ShoppingCart className='mr-2 h-5 w-5' /> Buy Now
+                                        </>
+                                }
+                            </Button>
+                            <Card className='border border-gray-200 shadow-sm p-6'>
+                                <CardHeader>
+                                    <CardTitle className='text-lg'>Book Details</CardTitle>
+                                </CardHeader>
+                                <CardContent className='grid gap-4'>
+                                    <div className='grid grid-cols-2 gap-4 text-sm'>
+                                        <div className='font-medium text-muted-foreground'>Subject/Tittle</div>
+                                        <div>{book.subject}</div>
+                                        <div className='font-medium text-muted-foreground'>Course</div>
+                                        <div>{book.classType}</div>
+                                        <div className='font-medium text-muted-foreground'>Category</div>
+                                        <div>{book.category}</div>
+                                        <div className='font-medium text-muted-foreground'>Author</div>
+                                        <div>{book.author}</div>
+                                        <div className='font-medium text-muted-foreground'>Edition</div>
+                                        <div>{book.edition}</div>
+                                        <div className='font-medium text-muted-foreground'>Condition</div>
+                                        <div>{book.condition}</div>
+                                    </div>
+                                </CardContent>
+                            </Card>
                         </div>
                     </div>
                 </div>
+                <div className='mt-8 grid gap-8 md:grid-cols-2'>
+                    <Card className='border-none shadow-md p-6'>
+                        <CardHeader>
+                            <CardTitle className='text-lg'>Description</CardTitle>
+                        </CardHeader>
+                        <CardContent className='space-y-4'>
+                            <p>{book.description}</p>
+                            <div className='border-t pt-4'>
+                                <h3 className='font-medium mb-2'>Our Community</h3>
+                                <p className='text-muted-foreground'>We're not just another shopping website where you buy from professional sellers - we are a vibrant community of students, book lovers across Bangladesh who deliver happiness to each other!</p>
+                            </div>
+                            <div className='flex items-center gap-4 text-sm text-muted-foreground'>
+                                <div>
+                                    Add Id: {book._id}
+                                </div>
+                                <div>
+                                    Posted: {formatDate(book.createdAt)}
+                                </div>
+                            </div>
+                        </CardContent>
+                    </Card>
+
+
+                    <Card className='border-none shadow-md p-6'>
+                        <CardHeader>
+                            <CardTitle className='text-lg'>Sell By:</CardTitle>
+                        </CardHeader>
+                        <CardContent className='space-y-4'>
+                            <div className='flex items-center justify-between'>
+                                <div className='flex items-center gap-3'>
+                                    <div className="h-12 w-12 rounded-full bg-blue-300 flex items-center justify-center">
+                                        <User2 className="h-6 w-6 text-blue-500" />
+                                    </div>
+
+                                    <div>
+                                        <div className='flex items-center gap-2'>
+                                            <span className='font-medium'>{book.seller.name}</span>
+                                            <Badge variant='secondary' className='text-green-600'><CheckCircle2 className='h-3 w-3 mt-2' />Verified</Badge>
+                                        </div>
+                                        <div className='flex items-center gap-2 text-sm text-muted-foreground'>
+                                            <MapPin className='h-4 w-4' />Barishal Sador
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            {
+                                book.seller.contact && (
+                                    <div className='flex items-center gap-2 text-sm'>
+                                        <MessageCircle className='h-4 w-4 text-blue-600' />
+                                        <span>Contact: {book.seller.contact}</span>
+                                    </div>
+                                )
+                            }
+                        </CardContent>
+                    </Card>
+                </div>
+
+                <section className='mt-16'>
+                    <h2 className='mb-8 text-2xl font-bold'>
+                        How does it work?
+                    </h2>
+                    <div className='grid gap-8 md:grid-cols-3'>
+                        {
+                            [{
+                                step: "Step 1",
+                                title: "Seller posts an Ad",
+                                description:
+                                    "Seller posts an ad on book chain to sell their used books.",
+                                image: { src: "/icons/ads.png", alt: "Post Ad" },
+                            },
+                            {
+                                step: "Step 2",
+                                title: "Buyer Pays Online",
+                                description:
+                                    "Buyer makes an online payment to book kart to buy those books.",
+                                image: { src: "/icons/pay_online.png", alt: "Payment" },
+                            },
+                            {
+                                step: "Step 3",
+                                title: "Seller ships the books",
+                                description: "Seller then ships the books to the buyer",
+                                image: { src: "/icons/fast-delivery.png", alt: "Shipping" },
+                            },].map((item, index) => (
+                                <Card className='bg-linear-to-br from-amber-50 to-amber-100 border-none p-6'>
+                                    <CardHeader>
+                                        <Badge className='w-fit mb-2'>{item.step}</Badge>
+                                        <CardTitle className='text-lg'>{item.title}</CardTitle>
+                                        <CardDescription>{item.description}</CardDescription>
+                                    </CardHeader>
+                                    <CardContent className='space-y-4'>
+                                        <Image src={item.image.src} alt={item.image.alt} width={120} height={120} className='mx-auto' />
+                                    </CardContent>
+                                </Card>
+                            ))
+                        }
+                    </div>
+                </section>
             </div>
         </div>
     )
